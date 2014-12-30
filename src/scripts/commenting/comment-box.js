7d0af601108e4fbb5cad1aa7dd8526638c8a6065
Michael Frye <michael_frye@intuit.com>
2014-12-29 17:32:24 -0800

    updated react reference

--- src/scripts/commenting/comment-box.js
+++ src/scripts/commenting/comment-box.js
@@ -1,4 +1,4 @@
-var React       = require('React');
+var React       = require('react');
 var API         = require('../api');
 var CommentList = require('./comment-list');
 var CommentForm = require('./comment-form');
