b327d2de38ea5f843e59fc4444539f904648cd87
Michael Frye <michael_frye@intuit.com>
2014-12-30 09:54:34 -0800

    added comments

--- src/scripts/example.js
+++ src/scripts/example.js
@@ -1,6 +1,13 @@
 
+// Create a CommentBox component
 var CommentBox = React.createClass({
+
+  // The render method is called automatically
+  // when the component is mounted to the DOM
   render: function() {
+
+    // This XMLish looking formatting is called JSX.
+    // It compiles to vanilla javascript via the included converter
     return (
       <div className="commentBox">
         Hello world!
@@ -9,6 +16,7 @@ var CommentBox = React.createClass({
   }
 });
 
+// Finds the element with the id "content" and mounts the CommentBox on it
 React.render(
   <CommentBox />,
   document.getElementById('content')
