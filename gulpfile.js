75b3f7d88c210bf5766d49e82bc25fc0b8654233
Michael Frye <michael_frye@intuit.com>
2014-12-30 13:19:43 -0800

    updated readme

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,17 @@
 var gulp        = require('gulp');
 var react       = require('gulp-react');
 
-// Compiles the JSX into raw javascript 
-// and puts the finished code into /dist
+// 1. This task compiles the JSX into raw javascript 
+//    and puts the finished code into the /dist folder
 gulp.task('js', function() {
   return gulp.src('./src/scripts/example.js')
     .pipe(react({harmony: true}))
     .pipe(gulp.dest('./dist/scripts'));
 });
 
-// These tasks copy over the other files (css / html)
-// They are separated so we can potentailly added LESS / SASS later on
+// 2. These tasks copy over the other files (css / html).
+//    They are separated so we can do things later on, like
+//    possibly adding LESS / SASS support.
 gulp.task('css', function() {
   return gulp.src('./src/css/*')
     .pipe(gulp.dest('./dist/css'))
@@ -21,8 +22,14 @@ gulp.task('html', function() {
     .pipe(gulp.dest('./dist'))
 });
 
-// The build task called via "gulp build"
+// 3. The build task called via "gulp build"
 gulp.task('build', ['js', 'css', 'html']);
 
-// The default task called via plain "gulp"
-gulp.task('default', ['build']);
+// 4. This task will watch the javascript and rebuild the app
+//    when changes are made
+gulp.task('watch', function(){
+  gulp.watch('./client/scripts/**/*', ['build'])
+});
+
+// 5. The default task called via plain "gulp"
+gulp.task('default', ['build', 'watch']);
