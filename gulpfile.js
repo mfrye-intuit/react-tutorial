460422ae38e0e241a4eb8f8fc61ec058bd66e9f4
Michael Frye <michael_frye@intuit.com>
2014-12-30 13:26:49 -0800

    updated readme and gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,14 @@ var react       = require('gulp-react');
 var browserify  = require('browserify');
 var reactify    = require('reactify');
 var source      = require('vinyl-source-stream');
-var shell       = require('gulp-shell');
 
-// We've updated this task to run browserify and compile our app
-// from the entry point "app.js"
+// 1. We've updated this task to run browserify and compile our app
+//    from the entry point "app.js"
 //
-// Before bundling the app, we run the transform function to convert
-// our JSX code into vanilla javascript
+//    Before bundling the app, we run the transform function to convert
+//    our JSX code into vanilla javascript
 //
-// All the code will be bundled into a single file called "bundle.js"
+//    All the code will be bundled into a single file called "bundle.js"
 gulp.task('js', function() {
   return browserify({
     entries: './src/scripts/app.js',
@@ -22,6 +21,9 @@ gulp.task('js', function() {
     .pipe(gulp.dest('./dist/scripts/'))
 });
 
+// 2. These tasks copy over the other files (css / html).
+//    They are separated so we can do things later on, like
+//    possibly adding LESS / SASS support.
 gulp.task('css', function() {
   return gulp.src('./src/css/*')
     .pipe(gulp.dest('./dist/css'))
@@ -32,12 +34,14 @@ gulp.task('html', function() {
     .pipe(gulp.dest('./dist'))
 });
 
-// Main testing tasks
-// This can be called via "jest" on the cli if jest is installed globally
-gulp.task('test', shell.task([
-  './node_modules/jest-cli/bin/jest.js test'
-]));
-
+// 3. The build task called via "gulp build"
 gulp.task('build', ['js', 'css', 'html']);
 
-gulp.task('default', ['build'])
+// 4. This task will watch the javascript and rebuild the app
+//    when changes are made
+gulp.task('watch', function(){
+  gulp.watch('./src/scripts/**/*', ['build'])
+});
+
+// 5. The default task called via plain "gulp"
+gulp.task('default', ['build', 'watch']);
