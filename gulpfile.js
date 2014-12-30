0bbeafad16e5c8a2086e56dfbb0660d5612bf585
Michael Frye <michael_frye@intuit.com>
2014-12-30 13:26:30 -0800

    updated readme and gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,13 @@ var browserify  = require('browserify');
 var reactify    = require('reactify');
 var source      = require('vinyl-source-stream');
 
-// We've updated this task to run browserify and compile our app
-// from the entry point "app.js"
+// 1. We've updated this task to run browserify and compile our app
+//    from the entry point "app.js"
 //
-// Before bundling the app, we run the transform function to convert
-// our JSX code into vanilla javascript
+//    Before bundling the app, we run the transform function to convert
+//    our JSX code into vanilla javascript
 //
-// All the code will be bundled into a single file called "bundle.js"
+//    All the code will be bundled into a single file called "bundle.js"
 gulp.task('js', function() {
   return browserify({
     entries: './src/scripts/app.js',
@@ -21,6 +21,9 @@ gulp.task('js', function() {
     .pipe(gulp.dest('./dist/scripts/'))
 });
 
+// 2. These tasks copy over the other files (css / html).
+//    They are separated so we can do things later on, like
+//    possibly adding LESS / SASS support.
 gulp.task('css', function() {
   return gulp.src('./src/css/*')
     .pipe(gulp.dest('./dist/css'))
@@ -31,8 +34,14 @@ gulp.task('html', function() {
     .pipe(gulp.dest('./dist'))
 });
 
-// The build task called via "gulp build"
+// 3. The build task called via "gulp build"
 gulp.task('build', ['js', 'css', 'html']);
 
-// The default task called via plain "gulp"
-gulp.task('default', ['build']);
+// 4. This task will watch the javascript and rebuild the app
+//    when changes are made
+gulp.task('watch', function(){
+  gulp.watch('./src/scripts/**/*', ['build'])
+});
+
+// 5. The default task called via plain "gulp"
+gulp.task('default', ['build', 'watch']);
