48a85a7e7c2cce9e4cd3c9e476fcc77fda5fb532
Michael Frye <michael_frye@intuit.com>
2014-12-30 12:48:38 -0800

    added comments

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,17 @@
 var gulp        = require('gulp');
 var react       = require('gulp-react');
 
-// Compiles the JSX into raw javascript 
-// and puts the finished code into /dist
+// This task compiles the JSX into raw javascript 
+// and puts the finished code into the /dist folder
 gulp.task('js', function() {
   return gulp.src('./src/scripts/example.js')
     .pipe(react({harmony: true}))
     .pipe(gulp.dest('./dist/scripts'));
 });
 
-// These tasks copy over the other files (css / html)
-// They are separated so we can potentailly added LESS / SASS later on
+// These tasks copy over the other files (css / html).
+// They are separated so we can do things later on, like
+// possibly adding LESS / SASS support.
 gulp.task('css', function() {
   return gulp.src('./src/css/*')
     .pipe(gulp.dest('./dist/css'))
--- src/scripts/example.js
+++ src/scripts/example.js
@@ -1,5 +1,13 @@
 
-// Nothing new added here
+// Nothing new added in this file.
+
+// In this step we are setting up our build process to precompile the JSX,
+// so we won't need to do it in the browser.
+//
+// We are using gulp as our build system, which is similar to grunt if you 
+// are familiar with that. Gulp enables us to create tasks to build / optimize
+// our frontend code for the browser.
+
 // Check out gulpfile.js to see our new build process
 
 var CommentBox = React.createClass({
