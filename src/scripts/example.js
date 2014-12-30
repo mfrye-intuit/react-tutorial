a506d1c76671acddfaeeec4cc37a9c67e6bf23e8
Michael Frye <michael_frye@intuit.com>
2014-12-30 10:34:30 -0800

    added comments

--- src/scripts/example.js
+++ src/scripts/example.js
@@ -56,23 +56,36 @@ var CommentList = React.createClass({
   }
 });
 
+// 1. Here we've built out our form
 var CommentForm = React.createClass({
+
+  // 3. When the form is submitted it creates and event (e)
   _handleSubmit: function(e) {
+    // First we prevent the browser default action from being triggered
     e.preventDefault();
 
+    // Then we collected the values using the ref attribute of the DOM element
     var author = this.refs.author.getDOMNode().value.trim();
     var comment = this.refs.comment.getDOMNode().value.trim();
 
+    // If either value is not defined, stop
     if (!comment || !author) {
       return;
     }
 
+    // Here is where we will put the actual adding of the new comment
     // TODO: Add to CommentsList and post to server
 
+    // We then clear out the inputs
     this.refs.author.getDOMNode().value = '';
     this.refs.comment.getDOMNode().value = '';
   },
 
+  // 2. We've added two inputs for the author and the comment,
+  //    and a button to submit the data.
+  //    We've also added the attribute defining which function to call
+  //    when the submit button is pressed.
+  //    We've also added some extra elements for styling using bootstrap.
   render: function() {
     return (
       <form className="commentForm" onSubmit={this._handleSubmit}>
